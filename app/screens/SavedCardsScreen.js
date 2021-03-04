6d231202292bb40972fcb174ba3a05fc66ed86c5
Nadin Tamer <nadin.tamer@gmail.com>
2021-03-03 21:59:21 -0800

    saved screen styling

--- app/screens/SavedCardsScreen.js
+++ app/screens/SavedCardsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, SafeAreaView, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { Text, View, SafeAreaView, ScrollView, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
 
 import Colors from '../themes/Colors';
 import ProfileButton from '../components/ProfileButton';
@@ -11,50 +11,59 @@ export default function SavedCardsScreen() {
 
   return (
     <SafeAreaView style={styles.homeContainer}>
-      <View style={styles.actionCard}>
-            <Image
-              source={require("../../assets/images/morningSickness.png")}
-              style={{ width: 60, height: 60}}
-            />
-          <Text style={styles.cardText}>{"\n"}
-          <Text style={styles.boldedText}>  Morning Sickness {"\n"}{"\n"}</Text>
-          <Text>  Despite its name, morning sickness does not {"\n"}  only occur in the morning.</Text>
-        </Text>
-      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.actionCard}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={require("../../assets/images/morningSickness.png")}
+                style={{ width: 60, height: 60}}
+              />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.boldedText}>Morning Sickness {"\n"}</Text>
+              <Text style={styles.cardText}>Despite its name, morning sickness does not only occur in the morning.</Text>
+            </View>
+        </View>
 
-      <View style={styles.actionCard}>
+        <View style={styles.actionCard}>
+          <View style={styles.imageContainer}>
             <Image
               source={require("../../assets/images/nausea.png")}
               style={{ width: 60, height: 60}}
             />
-          <Text style={styles.cardText}>{"\n"}
-          <Text style={styles.boldedText}>  Nausea Symptoms {"\n"}{"\n"}</Text>
-        <Text>  Morning sickness, vomiting, and more</Text>
-          </Text>
-      </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.boldedText}>Nausea Symptoms {"\n"}</Text>
+            <Text style={styles.cardText}>Morning sickness, vomiting, and more</Text>
+          </View>
+        </View>
 
-      <View style={styles.actionCard}>
+        <View style={styles.actionCard}>
+          <View style={styles.imageContainer}>
             <Image
               source={require("../../assets/images/covaude.png")}
               style={{ width: 60, height: 60}}
             />
-          <Text style={styles.cardText}>{"\n"}
-          <Text style={styles.boldedText}>  Covaude Syndrome {"\n"}{"\n"}</Text>
-        <Text>  The manifestation of pregnancy in men</Text>
-        </Text>
-      </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.boldedText}>Covaude Syndrome {"\n"}</Text>
+            <Text style={styles.cardText}>The manifestation of pregnancy in men</Text>
+          </View>
+        </View>
 
-      <View style={styles.actionCard}>
+        <View style={styles.actionCard}>
+          <View style={styles.imageContainer}>
             <Image
               source={require("../../assets/images/hyperemesis.png")}
               style={{ width: 60, height: 60}}
             />
-
-          <Text style={styles.cardText}>{"\n"}
-          <Text style={styles.boldedText}>  Hyperemis Gravidarum {"\n"}{"\n"}</Text>
-          <Text>  Extreme Morning Sickness: dehydration, {"\n"}  weight loss, and more</Text>
-        </Text>
-      </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.boldedText}>Hyperemis Gravidarum {"\n"}</Text>
+            <Text style={styles.cardText}>Extreme Morning Sickness: dehydration, weight loss, and more</Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -70,20 +79,40 @@ const styles = StyleSheet.create({
   },
   actionCard: {
     alignItems: 'center',
-    flexDirection: 'column',
+    flexDirection: 'row',
     backgroundColor: Colors.lightPurple,
     height: windowHeight * 0.15,
-    width: windowWidth * 0.9,
+    width: '100%',
     borderRadius: 20,
     marginTop: 30
   },
-  boldedText: {
-    fontWeight: "bold"
+  rowContainer: {
+    flexDirection: 'row',
   },
-pictureContainer: {
-  marginTop: 50,
-  marginBottom: 15,
-  alignItems: 'center',
-  justifyContent: 'space-between',
-},
+  imageContainer: {
+    flex: 1,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    flex: 4,
+    marginRight: 10,
+  },
+  boldedText: {
+    fontFamily: 'NunitoSans_700Bold',
+  },
+  cardText: {
+    fontFamily: 'NunitoSans_400Regular',
+  },
+  pictureContainer: {
+    marginTop: 50,
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  scrollView: {
+    width: '90%',
+    backgroundColor: Colors.white,
+  },
 });
