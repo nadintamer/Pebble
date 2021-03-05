809d81833043ee8d4f2fbed441952f1862dc350b
Nadin Tamer <nadin.tamer@gmail.com>
2021-03-04 20:10:14 -0800

    add settings screen

--- app/components/ProfileStackComponent.js
+++ app/components/ProfileStackComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, Image, View } from 'react-native';
+import { Text, Image, View, TouchableOpacity } from 'react-native';
 
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 
 import ProfileScreen from '../screens/ProfileScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 import EmergencyScreen from '../screens/EmergencyScreen';
 import FAQScreen from '../screens/FAQScreen';
 import SavedCardsScreen from '../screens/SavedCardsScreen';
@@ -22,14 +23,38 @@ export default function ProfileStackComponent() {
       <ProfileStack.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{
+        options={({ navigation, route }) => ({
           title: null,
           headerStyle: {
             backgroundColor: Colors.white,
             shadowColor: 'transparent',
           },
-        }}
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginRight: 20 }}
+              onPress={() => navigation.navigate('Settings')}>
+              <Image
+                source={require("../../assets/images/gear.png")}
+                style={{ width: 34, height: 34}}
+              />
+            </TouchableOpacity>
+          ),
+        })}
       />
+      <ProfileStack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: "Settings",
+          headerStyle: {
+            backgroundColor: Colors.white,
+            shadowColor: 'transparent',
+          },
+          headerTitleStyle: {
+            fontFamily: "Nunito_700Bold",
+            fontSize: 30,
+          }
+        }}/>
       <ProfileStack.Screen
         name="Emergency"
         component={EmergencyScreen}
